65d80f10ab8066a6dcca17992d825286b175ef13
Ashish Kumar <kumarashish79924@gmail.com>
2021-05-10 19:35:32 +0530

    𝐅𝐢𝐧𝐚𝐥 𝐏𝐫𝐨𝐣𝐞𝐜𝐭

--- src/main/java/com/ashish/TodoList/model/Users.java
+++ src/main/java/com/ashish/TodoList/model/Users.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Entity
+//
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
