770c4440ed7037a656adba0a862af3359b8568c6
Ashish Kumar <kumarashish79924@gmail.com>
2021-05-10 19:35:54 +0530

    𝐅𝐢𝐧𝐚𝐥 𝐏𝐫𝐨𝐣𝐞𝐜𝐭

--- src/main/java/com/ashish/TodoList/repository/UserRepository.java
+++ src/main/java/com/ashish/TodoList/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.ashish.TodoList.repository;
 import com.ashish.TodoList.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//
 @Repository
 public interface UserRepository extends JpaRepository<Users,Integer>
 {
